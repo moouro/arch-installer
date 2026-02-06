fde64293d2223969ef8abe8e1fb539c5c46c5d6b
mouro <dolbrebr@gmail.com>
2026-02-06 02:15:12 -0300

    feat: use Brazilian Portuguese and São Paulo timezone

--- install-arch.sh
+++ install-arch.sh
@@ -163,11 +163,12 @@ gum spin --title "Configuring system..." -- sleep 1
 PARTUUID_ROOT=$(blkid -s PARTUUID -o value "$PART_ROOT")
 
 arch-chroot /mnt /bin/bash <<EOF
-ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 hwclock --systohc
+echo "pt_BR.UTF-8 UTF-8" >> /etc/locale.gen
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "LANG=pt_BR.UTF-8" > /etc/locale.conf
 echo "$HOSTNAME" > /etc/hostname
 echo "127.0.0.1 localhost" >> /etc/hosts
 echo "::1       localhost" >> /etc/hosts
